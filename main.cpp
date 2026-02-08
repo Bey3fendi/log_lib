a3c3a84ffcab9e7fa149a2b97d963ddc0ec49a9a
furkan <furkanozdemirnew@gmail.com>
2026-02-08 13:24:58 +0000

    Add docker container and devcontainer.json

--- main.cpp
+++ main.cpp
@@ -4,34 +4,33 @@
 #include <iostream>
 #include <stdlib.h>
 
-#include "App.h"
-#include "util.h"
+#include "src/App.h"
 
 constexpr auto app_info = "log_daemon";
 
 int main() {
   auto result = EXIT_FAILURE;
 
-  std::cout << app_info << "start" << "\n";
+  std::cout << app_info << "start" << std::endl;
 
   try {
     log_daemon::App app{};
     if (!app.Init()) {
-      std::cout << "App init failed!" << "\n";
+      std::cout << "App init failed!" << std::endl;
     } else if (!app.Start()) {
-      std::cout << "App start failed!" << "\n";
+      std::cout << "App start failed!" << std::endl;
     } else {
-        auto signal = ::util::posix::WaitForSignal({SIGINT, SIGTERM});
+        // auto signal = ::util::posix::WaitForSignal({SIGINT, SIGTERM});
 
         if(app.Stop()) {
             result = EXIT_SUCCESS;
         }
 
-        std::cout << "App stopped cause of signal: " << signal << "\n";
+        std::cout << "App stopped cause of signal: " << signal << std::endl;
     }
   } catch (const std::exception &e) {
-    std::cout << "exception thrown: " << e.what() << "\n";
+    std::cout << "exception thrown: " << e.what() << std::endl;
   } catch (...) {
-    std::cout << "unknown exception thrown" << "\n";
+    std::cout << "unknown exception thrown" << std::endl;
   }
 }
--- src/App.cpp
+++ src/App.cpp
@@ -5,16 +5,16 @@
 namespace log_daemon {
 bool App::Init() {
   bool ret = false;
-  std::cout << "App initialized start" << "\n";
+  std::cout << "App initialized start" << std::endl;
 
   //...
   ret = true; //temporary
   if (ret) {
     std::cout << "App initialize gracefully flag returned as: "
-              << std::boolalpha << ret << "\n";
+              << std::boolalpha << ret << std::endl;
   } else {
     std::cout << "App initialize failed flag returned as: " << std::boolalpha
-              << ret << "\n";
+              << ret << std::endl;
   }
 
   return ret;
@@ -22,16 +22,16 @@ bool App::Init() {
 
 bool App::Start() {
   bool ret = false;
-  std::cout << "App start" << "\n";
+  std::cout << "App start" << std::endl;
 
   //...
   ret = true; //temporary
   if (ret) {
     std::cout << "App start gracefully flag returned as: " << std::boolalpha
-              << ret << "\n";
+              << ret << std::endl;
   } else {
     std::cout << "App start failed flag returned as: " << std::boolalpha << ret
-              << "\n";
+              << std::endl;
   }
 
   return ret;
@@ -39,16 +39,16 @@ bool App::Start() {
 
 bool App::Stop() {
   bool ret = false;
-  std::cout << "App stop" << "\n";
+  std::cout << "App stop" << std::endl;
 
   //...
   ret = true; //temporary
   if (ret) {
     std::cout << "App stop gracefully flag returned as: " << std::boolalpha
-              << ret << "\n";
+              << ret << std::endl;
   } else {
     std::cout << "App stop failed flag returned as: " << std::boolalpha << ret
-              << "\n";
+              << std::endl;
   }
 
   return ret;
